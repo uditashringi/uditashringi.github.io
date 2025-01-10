43411ed62b3247c2e62873e61a13450ab0e4dd61
Abhishek Shringi <abhishekshringi@Abhisheks-MacBook-Pro.local>
2025-01-09 22:29:26 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+
+import React from "react";
+// import { useState } from "react";
+import "./App.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const App = () => {
+  // const [theme, setTheme] = useState("cupcake");
+  const theme = "cupcake";
+
+  // const toggleTheme = (newTheme) => {
+  //   setTheme(newTheme);
+  // };
+
+  const projects = [
+    {
+      name: "Tapestry",
+      description: "Decorative Textile Art traditionally made by weabing threads on a loom. It consists of two main thread types:" +
+              "warp threads( vertical ) that form the base and weft threads( horizontal ) that are woven through the warp to create designs or scenes.",
+      link: "/pdfs/tapestry.pdf"
+    },
+  ];
+
+  return (
+    <div className={`app theme-${theme}`}>
+      <header className="header">
+        <h1>Udita Shringi</h1>
+        <p>Sophomore pursuing B.Des in Fashion/Apparel Design</p>
+        {/* <button className="theme-button" onClick={() => toggleTheme(theme === "cupcake" ? "dark" : "cupcake")}>Change Theme</button> */}
+      </header>
+
+      <section className="profile">
+        <img
+          src="/images/udita.jpg"
+          alt="Profile"
+          className="profile-picture"
+        />
+        <ul className="contact-info">
+          <li>
+            <a href="https://github.com/uditashringi">
+              <i className="fab fa-github"></i> uditashringi
+            </a>
+          </li>
+          <li>
+            <a href="mailto:uditashringi.fashion@gmail.com">
+              <i className="fas fa-envelope"></i> uditashringi.fashion@gmail.com
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/udita-shringi-24641824a/">
+              <i className="fab fa-linkedin"></i> udita-shringi
+            </a>
+          </li>
+        </ul>
+        <a href="/pdfs/uditaresume.pdf">
+          <button className="download-button">View Resume</button>
+        </a>
+      </section>
+
+      <section className="projects">
+        <h2>Portfolio</h2>
+        <div className="project-list">
+          {projects.map((project, index) => (
+            <a href={project.link} key={index} className="project-link">
+              <div key={index} className="project-card">
+                <h3>{project.name}</h3>
+                <p>{project.description}</p>
+              </div>
+            </a>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
+
